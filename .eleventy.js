a3e014597d27381561f677f5e2cd3efb6fc70473
Duncan McAlester <duncan@rocketcom.com>
2020-05-20 13:56:13 -0700

    Fixed missing passthrough copy for favicon

--- .eleventy.js
+++ .eleventy.js
@@ -74,6 +74,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("fonts");
+  eleventyConfig.addPassthroughCopy("favicon");
 
   //
   eleventyConfig.setBrowserSyncConfig({
