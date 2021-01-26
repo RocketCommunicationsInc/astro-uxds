1b3249e4d73d89d2315b041d2c305e07efd26ca3
Duncan McAlester <duncan@rocketcom.com>
2021-01-26 15:57:05 -0800

    Consolidated Eleventy.js vars in to meta.js
    Moved the git.js vars into meta.js. One it centralized the location of
    the global vars consumed by eleventy and two there was a YAML git
    var that appeared to be conflicting with the .js var. Fortunately
    with no dire condequences.

--- _content/_data/meta.js
+++ _content/_data/meta.js
@@ -1,6 +1,6 @@
 module.exports = {
   version: "5.0",
   copyright: new Date().getFullYear(),
-  branch: process.env.BRANCH,
-  environment: process.env
+  repo: process.env.REPOSITORY_URL,
+  branch: process.env.BRANCH
 };
